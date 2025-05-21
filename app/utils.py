c646198b57a345aea9193ad82b8d83be8a60bcb3
moablex <moablex.ablantedaniel888@gmail.com>
2025-05-21 08:25:43 +0300

    Fix: fixing eror in app/utils for streamlite app

--- app/utils.py
+++ app/utils.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 def load_data(country):
-    return pd.read_csv(f"../data/{country}_clean.csv", index_col="Timestamp", parse_dates=True)
+    return pd.read_csv(f"../data/{country}.csv", index_col="Timestamp", parse_dates=True)
 
 def get_ghi_summary(df):
     return {"mean": df["GHI"].mean(), "median": df["GHI"].median(), "std": df["GHI"].std()}
